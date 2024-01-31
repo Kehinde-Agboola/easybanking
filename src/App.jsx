caf6898a285c8b85a8eb8e6ab2b0778b1879a9dc
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2024-01-31 09:55:47 +0100

    created component folder in src

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from "./components";
+
 export default function App() {
   return (
-    <h1 className="h-screen flex justify-center items-center text-[100px] underline font-bold text-red-800 ">
-      Good to Go!
-    </h1>
+    <div>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element />
+          <Route path="/" element />
+          <Route path="/" element />
+          <Route path='/' element />
+          <Route path="/" element />
+        </Route>
+      </Routes>
+    </div>
   );
 }
+
+
+<div className={`relative z-0`}>
+        {/* <header className={`flex items-center justify-center bg-sky-700 w-full fixed top-0 z-20`}>
+          <Navbar />
+        </header> */}
+        <ScrollToTop />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="officials" element={<AbtOfficials />} />
+            <Route path="units" element={<OurUnits />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="admin" element={<Admin />} />
+            {user && user.admin === true ? (
+            <Route path='admin/dashboard/*' element={<AdminDashboard />} />
+           ) : ( 
+             <Route path='admin/dashboard/*' element={<Navigate to="/" />} /> 
+           )} 
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      
+    </div>
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Navabr from "./Navabr";
+import Outlet from "./Outlet";
+import Footer from "./Footer";
+import Outlet from "./Outlet";
+import Offers from "./Offers";
+
+
+export {
+    Navabr,
+    Outlet,
+    Footer,
+    Outlet,
+    Offers
+}
